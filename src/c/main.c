1dfcf54300d66db97936e16e5428b935e32cdeba
antonioasaro <antonioasaro@users.noreply.github.com>
2016-09-15 10:48:29 -0400

    Updated stock price colour

--- src/c/main.c
+++ src/c/main.c
@@ -244,7 +244,7 @@ void handle_init(void) {
     stock_text_layer = text_layer_create(GRect(96, 24, 40, 30));
     text_layer_set_font(stock_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     text_layer_set_text_alignment(stock_text_layer, GTextAlignmentRight);
-    text_layer_set_text_color(stock_text_layer, GColorIcterine);	
+    text_layer_set_text_color(stock_text_layer, GColorSpringBud);	
     text_layer_set_background_color(stock_text_layer, GColorBlack);
     text_layer_set_text(stock_text_layer, "$0.00");
     layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(stock_text_layer));	
